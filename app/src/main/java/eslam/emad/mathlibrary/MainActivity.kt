ce3abf369ec07fa13a9a06706dc792a3e3ff1b8e
eslam <eslamemad903@gmail.com>
2020-12-15 14:12:02 +0200

    first commit

---
+++ app/src/main/java/eslam/emad/mathlibrary/MainActivity.kt
@@ -0,0 +1,26 @@
+package eslam.emad.mathlibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import eslam.emad.library.MathOperations.divide
+import eslam.emad.library.MathOperations.minus
+import eslam.emad.library.MathOperations.multiply
+import eslam.emad.library.MathOperations.plus
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.tv)
+
+        val firstNum = 20
+        val secondNum = 10
+        val plus = plus(firstNum, secondNum)
+        val minus = minus(firstNum, secondNum)
+        val multiply = multiply(firstNum, secondNum)
+        val divide = divide(firstNum, secondNum)
+
+        textView.text = "$plus \n $minus \n $multiply \n $divide"
+    }
+}
---
+++ library/src/main/java/eslam/emad/library/MathOperations.kt
@@ -0,0 +1,20 @@
+package eslam.emad.library
+
+object MathOperations {
+
+    fun plus(firstNumber: Int, secondNumber: Int): Int {
+        return firstNumber + secondNumber
+    }
+
+    fun minus(firstNumber: Int, secondNumber: Int): Int {
+        return firstNumber - secondNumber
+    }
+
+    fun multiply(firstNumber: Int, secondNumber: Int): Int {
+        return firstNumber * secondNumber
+    }
+
+    fun divide(firstNumber: Int, secondNumber: Int): Int {
+        return firstNumber / secondNumber
+    }
+}
